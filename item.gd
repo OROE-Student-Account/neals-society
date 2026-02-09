4b29477e7f79c768c672aa4dbf9ebd869e110540
NMSehgal <nsehgal27@punahou.edu>
2026-02-08 18:04:43 -1000

    Fixed item pickup
    Made it so it disappears when picked up

--- item.gd
+++ item.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
-
 var picked_up = false
 
 func pickup():
+	if picked_up:
+		return  # safety check so we don't pick up twice
+	picked_up = true
 	visible = false
+	$CollisionShape2D.disabled = true  # disable collision so it can't be triggered again
 	Utils.get_scene_manager().transition_to_dialogue("Pick up", "Leave")
-
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -14,5 +16,5 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_body_exited(body: Node2D) -> void:
-	if not picked_up:
-		visible = true
+	# nothing necessary rn
+	pass
