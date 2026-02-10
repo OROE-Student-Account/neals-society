e7be93d2a9e741effd6abe4c67491fedc5d7c055
NMSehgal <nsehgal27@punahou.edu>
2026-02-10 10:57:04 -1000

    demo route 1 + failed item system
    I made a route 1 demo and I tried to get a functioning item system but i cant so for now its just it picks up with both dialogue options. I also added Ilyas's house to the files.

--- item.gd
+++ item.gd
@@ -1,20 +1,23 @@
 extends Area2D
 
-var picked_up = false
+var picked_up := false
+var dialogue_open := false
+
+func _on_body_entered(body):
+	if picked_up or dialogue_open:
+		return
+	if body.is_in_group("Player"):
+		dialogue_open = true
+		Utils.get_scene_manager().transition_to_dialogue(
+			"Pick up",
+			"Leave",
+		)
+		pickup()
 
 func pickup():
-	if picked_up:
-		return  # safety check so we don't pick up twice
 	picked_up = true
 	visible = false
-	$CollisionShape2D.disabled = true  # disable collision so it can't be triggered again
-	Utils.get_scene_manager().transition_to_dialogue("Pick up", "Leave")
+	$CollisionShape2D.disabled = true
 
-
-func _on_body_entered(body: Node2D) -> void:
-	pickup()
-
-
-func _on_body_exited(body: Node2D) -> void:
-	# nothing necessary rn
-	pass
+func on_dialogue_closed():
+	dialogue_open = false
