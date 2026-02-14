2286feffc08aade01bab977201c75ed5753b0cd2
OROE-Student-Account <oroe28@punahou.edu>
2026-02-13 18:49:07 -1000

    Fixed problem
    An issue occurred when merging the dialogue branch, but I fixed it now. For Future reference. Try not to modify the town or any commonly changed scene in a branch.

--- dialogue_node.gd
+++ dialogue_node.gd
@@ -2,8 +2,8 @@ extends Node
 class_name DialogueNode
 
 @export var is_end_node = false # determines if this option ends the conversation
-@export var text = "" # text to show when this option is selected
 @export var option_name = "" # if this has a parent, this is the option name
+@export var text = "" # text to show when this option is selected
 @export var function = "" # what to do after this is selected
 
 
@@ -13,3 +13,7 @@ func get_children_nodes() -> Array[DialogueNode]:
 		if child is DialogueNode:
 			children.append(child)
 	return children
+
+
+func begin_from_self():
+	Utils.get_scene_manager().transition_to_dialogue(self, self)
--- item.gd
+++ item.gd
@@ -1,25 +1,12 @@
 extends Area2D
 
 var picked_up := false
-var dialogue_open := false
-
-func _on_body_entered(body):
-	if picked_up or dialogue_open:
-		return
-	if body.is_in_group("Player"):
-		dialogue_open = true
-		Utils.get_scene_manager().transition_to_dialogue(
-			"Pick up",
-			"Leave",
-		)
-		pickup()
 
 @onready var dialogue_root = $"Pickup Dialogue"
 
 func pickup():
 	if !picked_up:
 		return
-	picked_up = true
 	visible = false
 	$CollisionShape2D.disabled = true  # disable collision so it can't be triggered again
 
