ff29cf0dc5446715b8779bf01ed685d31debd341
OROE-Student-Account <oroe28@punahou.edu>
2026-03-21 23:54:16 -1000

    Added shards
    Improved the crafting screen and added shards

--- Scripts/CraftScreen.gd
+++ Scripts/CraftScreen.gd
@@ -3,11 +3,14 @@ extends Node2D
 enum Page { MAIN, SWITCH }
 var page = Page.MAIN
 
-enum Options { SWITCH1, SWITCH2, CANCEL }
+enum Options { SWITCH1, SWITCH2, CRAFT, CANCEL }
 var selected_option = 0
 
 @onready var buttons = $Buttons
 @onready var shard_select = $ShardSelect
+
+var shard1 = ""
+var shard2 = ""
 
 
 var types = [
@@ -25,23 +28,24 @@ var types = [
 ]
 
 var border_positions = [
-	Vector2(64,48),
-	Vector2(96,48),
-	Vector2(128,48),
-	Vector2(160,48),
-	Vector2(80,80),
-	Vector2(112,80),
-	Vector2(144,80),
-	Vector2(64,112),
-	Vector2(96,112),
-	Vector2(128,112),
-	Vector2(160,112)
+	Vector2(72,60),
+	Vector2(104,60),
+	Vector2(136,60),
+	Vector2(168,60),
+	Vector2(88,92),
+	Vector2(120,92),
+	Vector2(152,92),
+	Vector2(72,124),
+	Vector2(104,124),
+	Vector2(136,124),
+	Vector2(168,124)
 ]
 
 
 func _ready() -> void:
 	update_buttons()
 	update_screen()
+	load_shards()
 
 
 
@@ -58,30 +62,76 @@ func update_buttons():
 			for butt in buttons.get_children():
 				butt.frame = 0
 			buttons.get_child(selected_option).frame = 1
+			if shard1 != "":
+				$Control/Shard1.texture = load("res://Assets/Items/"+shard1+" Shard.png")
+			if shard2 != "":
+				$Control/Shard2.texture = load("res://Assets/Items/"+shard2+" Shard.png")
 		Page.SWITCH:
 			shard_select.get_node("Border").position = border_positions[selected_option]
+			shard_select.get_node("Label").text = "Count: "+str(count_shard(types[selected_option]))
+
+
+func select_shard():
+	page = Page.SWITCH
+	selected_option = 0
+	update_screen()
+	update_buttons()
+	
+	while page == Page.SWITCH:
+		await get_tree().process_frame
+	
+	var shard = selected_option
+	
+	selected_option = 0
+	update_screen()
+	update_buttons()
+	
+	return types[shard]
+
+func load_shards():
+	for i in range(len(types)):
+		var shard = Sprite2D.new()
+		shard.texture = load("res://Assets/Items/"+types[i]+" Shard.png")
+		shard.position = border_positions[i]+Vector2(1,0)
+		shard.scale = Vector2(0.916, 0.916)
+		shard_select.add_child(shard)
+
+func count_shard(item):
+	var count = 0
+	while Utils.remove_from_inventory(item+" Shard"):
+		count += 1
+	
+	for i in range(count):
+		Utils.add_to_inventory(item+" Shard")
+	
+	return count
 
 
 func _input(event):
 	match page:
 		Page.MAIN:
 			if event.is_action_pressed("ui_up"):
-				if selected_option == Options.CANCEL:
-					selected_option = Options.SWITCH2
+				if selected_option > Options.SWITCH2:
+					selected_option -= 2
 				update_buttons()
 				
 			elif event.is_action_pressed("ui_down"):
-				selected_option = Options.CANCEL
+				if selected_option < Options.CRAFT:
+					selected_option += 2
 				update_buttons()
 				
 			elif event.is_action_pressed("ui_right"):
 				if selected_option == Options.SWITCH1:
 					selected_option = Options.SWITCH2
+				elif selected_option == Options.CRAFT:
+					selected_option = Options.CANCEL
 				update_buttons()
 				
-			elif event.is_action_pressed("ui_right"):
+			elif event.is_action_pressed("ui_left"):
 				if selected_option == Options.SWITCH2:
 					selected_option = Options.SWITCH1
+				elif selected_option == Options.CANCEL:
+					selected_option = Options.CRAFT
 				update_buttons()
 				
 			elif event.is_action_pressed("x") or (event.is_action_pressed("z") and selected_option == Options.CANCEL):
@@ -89,15 +139,14 @@ func _input(event):
 				
 			elif event.is_action_pressed("z"):
 				if selected_option == Options.SWITCH1:
-					page = Page.SWITCH
-					selected_option = 0
-					update_screen()
+					shard1 = await select_shard()
 					update_buttons()
 				elif selected_option == Options.SWITCH2:
-					page = Page.SWITCH
-					selected_option = 0
-					update_screen()
+					shard2 = await select_shard()
+					selected_option = Options.SWITCH2
 					update_buttons()
+				elif selected_option == Options.CRAFT:
+					pass
 		Page.SWITCH:
 			if event.is_action_pressed("ui_up"):
 				if selected_option > 7:
@@ -105,20 +154,22 @@ func _input(event):
 				elif selected_option > 3:
 					selected_option -= 3
 				update_buttons()
-				
 			elif event.is_action_pressed("ui_down"):
 				if selected_option < 3:
 					selected_option += 4
 				elif selected_option < 7:
 					selected_option += 3
 				update_buttons()
-				
 			elif event.is_action_pressed("ui_right"):
 				if selected_option != 3 and selected_option != 6 and selected_option != 10:
 					selected_option += 1
 				update_buttons()
-				
 			elif event.is_action_pressed("ui_left"):
 				if selected_option != 0 and selected_option != 4 and selected_option != 7:
 					selected_option -= 1
 				update_buttons()
+			
+			elif event.is_action_pressed("z"):
+				if count_shard(types[selected_option]) > 0:
+					if shard1 != types[selected_option] and shard2 != types[selected_option]:
+						page = Page.MAIN
--- Scripts/HarvestScreen.gd
+++ Scripts/HarvestScreen.gd
@@ -34,36 +34,48 @@ func crush():
 	pause = true
 	
 	var party = Utils.get_party()
-	var grammarite = Utils.get_grammarite_details(party[slot_to_crush]["Name"])
-	var shard_type = grammarite["Stats"]["Types"].pick_random()
-	Utils.add_to_inventory(shard_type+" Shard")
 	
-	for i in range(slot_to_crush, 5): 
-		party[i] = party[i+1]
-	
-	party[5] = {
-		"Health": 0.0,
-		"Item": "",
-		"Level": 1.0,
-		"Moves": [],
-		"Name": "",
-		"Nickname": "",
-		"PP": []
-	}
-	
-	Utils.set_party(party)
+	var count = 0
+	for g in party:
+		if g["Name"] != "":
+			count += 1
 	
 	animation.visible = true
-	animation.get_node("AnimationPlayer").play("Crush")
-	await get_tree().create_timer(4.0).timeout
+	
+	if count > 1:
+		var grammarite = Utils.get_grammarite_details(party[slot_to_crush]["Name"])
+		var shard_type = grammarite["Stats"]["Types"].pick_random()
+		Utils.add_to_inventory(shard_type+" Shard")
+		
+		for i in range(slot_to_crush, 5): 
+			party[i] = party[i+1]
+		
+		party[5] = {
+			"Health": 0.0,
+			"Item": "",
+			"Level": 1.0,
+			"Moves": [],
+			"Name": "",
+			"Nickname": "",
+			"PP": []
+		}
+		
+		Utils.set_party(party)
+		
+		animation.get_node("AnimationPlayer").play("Crush")
+		await get_tree().create_timer(4.0).timeout
+		
+		animation.get_node("Label").text = "You got a "+shard_type+" Shard!"
+	else:
+		animation.get_node("Label").text = "Get a larger party before harvesting shards."
 	
 	animation.get_node("AnimationPlayer").play("Label")
-	animation.get_node("Label").text = "You got a "+shard_type+" Shard!"
 	await get_tree().create_timer(1.5).timeout
 	
 	animation.visible = false
 	pause = false
 	selected_option = 0
+	slot_to_crush = 0
 	update_buttons()
 	update_display()
 
